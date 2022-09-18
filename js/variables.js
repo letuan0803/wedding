d2259678f55461b90c173f7a8eb6d521a76ab00a
duchuy0990 <32492519+duchuy0990@users.noreply.github.com>
2022-09-18 19:53:20 +0700

    fix giao dien

--- js/variables.js
+++ js/variables.js
@@ -16,9 +16,7 @@ var hero_full_screen = true;                    //If true, the hero section will
 
 //HERO BACKGROUND SLIDESHOW IMAGES
 var slidehow_images = [
-    "https://via.placeholder.com/1920x1080.png",
-    "https://via.placeholder.com/1920x1080.png",
-    "https://via.placeholder.com/1920x1080.png"
+    "images/images/slide_show_1.jpg"
 ];
 
 //TIMELINE PARALLAX
@@ -42,7 +40,7 @@ var countdown_end_msg = "Event Started!";       //Message to display when the co
 //GOOGLE MAP VARIABLES
 var map_canvas_id = "map_canvas";               //The HTML "id" of the map canvas
 var map_color = "#8eaeba";                      //Google map color
-var map_initial_zoom = 20;                      //The initial zoom when Google map loads
+var map_initial_zoom = 15;                      //The initial zoom when Google map loads
 var map_initial_latitude = 21.00081859629792;           //Google map initial Latitude. If "null", the latitude of the first marked will be used
 var map_initial_longitude = 105.87097199752556;        //Google map initial Longitude. If "null", the longitude of the first marked will be used
 var use_default_map_style = false;              //If true, default map style will be used
@@ -55,7 +53,7 @@ var map_markers = [
         "latitude": 21.00081859629792,
         "longitude": 105.87097199752556,
         "icon": "fas fa-glass-cheers", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-        "infoWindow": "Hà Nội Sahul A25 Hotel & Spa"
+        "infoWindow": "<b>Hà Nội Sahul A25 Hotel & Spa <br> 684 Minh Khai, HBT, Hà Nội</b>"
     },
     {
         "title": "Accommodation 2",
